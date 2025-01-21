4071c063f78dda8872150f6a6d31c4d130665381
Sophia Duan <duansophia8@gmail.com>
2025-01-20 20:31:44 -0500

    added basic eye design in css

--- script.js
+++ script.js
@@ -4,23 +4,31 @@ window.addEventListener("load", function(){
 
 })
 
-const audio = document.getElementById('backgroundMusic');
-const musicStatus = document.getElementById('musicStatus');
-const circle = document.getElementById('circle');
-let isPlaying = true; 
+// const audio = document.getElementById('backgroundMusic');
+// const musicStatus = document.getElementById('musicStatus');
+// const circle = document.getElementById('circle');
+// let isPlaying = true; 
 
-musicStatus.addEventListener('click', () => {
-if (isPlaying) {
-    audio.pause(); 
-    musicStatus.textContent = "ON"; 
-    circle.style.backgroundColor = '#ff2b39';
+// musicStatus.addEventListener('click', () => {
+// if (isPlaying) {
+//     audio.pause(); 
+//     musicStatus.textContent = "ON"; 
+//     circle.style.backgroundColor = '#ff2b39';
 
 
-} else {
-    audio.play(); 
-    musicStatus.textContent = "OFF"; 
-    circle.style.backgroundColor = '#69c29e';
+// } else {
+//     audio.play(); 
+//     musicStatus.textContent = "OFF"; 
+//     circle.style.backgroundColor = '#69c29e';
 
-}
-isPlaying = !isPlaying; // Toggle the state
+// }
+// isPlaying = !isPlaying; // Toggle the state
+// });
+
+const follow = document.querySelector(".follow");
+
+document.addEventListener('mousemove', (event) => {
+    const { clientX, clientY } = event;
+    follow.style.left = `${clientX - follow.offsetWidth / 2}px`;
+    follow.style.top = `${clientY - follow.offsetHeight / 2}px`;
 });
