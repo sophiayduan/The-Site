02cde17e3a1116a96f8082d775c79ad79f3f1c2e
Sophia Duan <duansophia8@gmail.com>
2025-01-24 20:08:21 -0500

    minor fixes + mobile view

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 This is my personal site! 
 You can visit it here :
+Hi hack clubbers! thansk for checking out my site. 
+
+ideally you should be viewing this site on a laptop/computer, to fully experience the effects (hover effects, mouse tracking, etc.)
+
+This site is in a forever building stage as a learn more about web developpment and apply my skills here, and share my projecs un a blog style post! 
+
+small note: the eye that looks at your mouse was a temporary addition, something to fill the space to "finish" the website so I can study for exams peacefully. 
+
+This is mention on the site but i'll reiterate: 
+This website was inspired by comic books, 
--- script.js
+++ script.js
@@ -2,33 +2,29 @@ var loader = document.getElementById("preloader");
 window.addEventListener("load", function(){
     loader.style.display = "none";
 
-})
-
-// const audio = document.getElementById('backgroundMusic');
-// const musicStatus = document.getElementById('musicStatus');
-// const circle = document.getElementById('circle');
-// let isPlaying = true; 
-
-// musicStatus.addEventListener('click', () => {
-// if (isPlaying) {
-//     audio.pause(); 
-//     musicStatus.textContent = "ON"; 
-//     circle.style.backgroundColor = '#ff2b39';
-
-
-// } else {
-//     audio.play(); 
-//     musicStatus.textContent = "OFF"; 
-//     circle.style.backgroundColor = '#69c29e';
-
-// }
-// isPlaying = !isPlaying; // Toggle the state
-// });
-
-const follow = document.querySelector(".follow");
-
-document.addEventListener('mousemove', (event) => {
-    const { clientX, clientY } = event;
-    follow.style.left = `${clientX - follow.offsetWidth / 2}px`;
-    follow.style.top = `${clientY - follow.offsetHeight / 2}px`;
 });
+
+const audio = document.getElementById('backgroundMusic');
+const musicStatus = document.getElementById('musicStatus');
+const circle = document.getElementById('circle');
+let isPlaying = false; 
+
+musicStatus.addEventListener('click', () => {
+    if (isPlaying) {
+        audio.pause(); 
+        musicStatus.textContent = "OFF"; 
+        circle.style.backgroundColor = '#ff2b39';
+
+
+    } else {
+        audio.play(); 
+        musicStatus.textContent = "ON"; 
+        circle.style.backgroundColor = '#69c29e';
+
+    }
+    isPlaying = !isPlaying;
+});
+
+
+
+
