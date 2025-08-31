c6573ededa3d91cae1772f4ff8836d23cbf97e10
Sophia Duan <duansophia8@gmail.com>
2025-08-30 21:08:14 -0400

    make tabs function

--- script.js
+++ script.js
@@ -1,42 +1,70 @@
 dragElement(document.getElementById("jpeg"));
+dragElement(document.getElementById("endev"));
+dragElement(document.getElementById("pnp"));
+
 
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
   if (document.getElementById(elmnt.id + "header")) {
-    /* if present, the header is where you move the DIV from:*/
     document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
   } else {
-    /* otherwise, move the DIV from anywhere inside the DIV:*/
     elmnt.onmousedown = dragMouseDown;
   }
 
   function dragMouseDown(e) {
     e = e || window.event;
     e.preventDefault();
-    // get the mouse cursor position at startup:
     pos3 = e.clientX;
     pos4 = e.clientY;
     document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
     document.onmousemove = elementDrag;
   }
 
   function elementDrag(e) {
     e = e || window.event;
     e.preventDefault();
-    // calculate the new cursor position:
     pos1 = pos3 - e.clientX;
     pos2 = pos4 - e.clientY;
     pos3 = e.clientX;
     pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+
+
+    let newTop = elmnt.offsetTop - pos2;
+    let newLeft = elmnt.offsetLeft - pos1;
+
+    const maxTop = window.innerHeight - elmnt.offsetHeight;
+    const maxLeft = window.innerWidth - elmnt.offsetWidth;
+
+    if (newTop < 0) newTop = 0;
+    if (newLeft < 0) newLeft = 0;
+    if (newTop > maxTop) newTop = maxTop;
+    if (newLeft > maxLeft) newLeft = maxLeft;
+
+    elmnt.style.top = newTop + "px";
+    elmnt.style.left = newLeft + "px";
   }
 
   function closeDragElement() {
-    /* stop moving when mouse button is released:*/
     document.onmouseup = null;
     document.onmousemove = null;
   }
 }
+
+//the code above is thanks to w3schools
+let z_index = 10;
+
+const tabs = document.querySelectorAll('.tab')
+const projects = document.querySelectorAll('.proj')
+
+tabs.forEach((tab, i) => {
+  tab.addEventListener('mousedown', () => {
+    z_index++
+    tab.style.zIndex = z_index
+  })
+
+
+  projects[i].addEventListener('mousedown', () => {
+    z_index++;
+    tab.style.zIndex = z_index;
+  });
+});
